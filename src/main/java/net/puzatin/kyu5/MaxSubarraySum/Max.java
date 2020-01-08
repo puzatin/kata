b643251708f6fe3d09720075e8da050d17b0a0b2
Oleg Yudin <puzatin@gmail.com>
2020-01-08 22:37:21 +0700

    Refactor MaxSubArraySum

--- src/main/java/net/puzatin/kyu5/MaxSubarraySum/Max.java
+++ src/main/java/net/puzatin/kyu5/MaxSubarraySum/Max.java
@@ -1,19 +1,15 @@
 package net.puzatin.kyu5.MaxSubarraySum;
 
+// the task: https://www.codewars.com/kata/54521e9ec8e60bc4de000d6c
+
 public class Max {
     public static int sequence(int[] arr) {
 
+        int current = 0;
         int max = 0;
-        int count = 0;
-        for (int i = 0; i < arr.length; i++) {
-            int submax = 0;
-            int sum = 0;
-            for (int j = count; j < arr.length; j++){
-                sum += arr[j];
-                if (sum > submax) submax = sum;
-            }
-            if(submax > max) max = submax;
-            count++;
+        for (int i : arr) {
+            current = Math.max(0, current + i);
+            max = Math.max(max, current);
         }
         return max;
     }
